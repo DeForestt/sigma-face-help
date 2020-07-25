000f297ddcf3faab9a8b2f91a8e9937c958f4ab6
DeForestt Thompson <deforesttthompson@gmail.com>
2020-07-25 00:28:28 -0400

    Add project files.

---
+++ sigma-face-help.API/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace sigma_face_help.API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
