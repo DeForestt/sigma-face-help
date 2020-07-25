1513f853dc88216d4b1bf0d6a477dea7bffb3a56
DeForestt Thompson <deforesttthompson@gmail.com>
2020-07-25 02:12:09 -0400

    Major Refactoring

---
+++ FaceHelp.Service.Library/Entities/CaregiverEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceHelp.Service.Library.Entities
+{
+    class CaregiverEntity
+    {
+        public string UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public List<PatientEntity> Patients { get; set; }
+    }
+}
--- FaceHelp.Service.Library/Entities/PatientEntity.cs
+++ FaceHelp.Service.Library/Entities/PatientEntity.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
-namespace sigma_face_help.API.Models
+namespace FaceHelp.Service.Library.Entities
 {
-    public class PatientModel
+    public class PatientEntity
     {
         public string UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
         public int Age { get; set; }
         public List<Condition> HealthConditions { get; set; }
     }
