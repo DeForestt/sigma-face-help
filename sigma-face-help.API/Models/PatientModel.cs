3d76a00183fb62e073114b88b650fd1aae9d21b8
DeForestt Thompson <deforesttthompson@gmail.com>
2020-07-25 01:12:13 -0400

    Added modle for patient..

---
+++ sigma-face-help.API/Models/PatientModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sigma_face_help.API.Models
+{
+    public class PatientModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public List<Condition> HealthConditions { get; set; }
+    }
+
+    // Possible conditions
+    public enum Condition
+    {
+        Arthritis,
+        Hypertension,
+        Glaucoma
+    }
+}
